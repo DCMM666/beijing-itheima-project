5e576fda9807d54a575d4efb06424e0028f53b01
东辰末末 <2324110064@qq.com>
2025-07-31 15:40:56 +0800

    first commit

---
+++ day01-demo/src/com/itheima/variable/Demo.java
@@ -0,0 +1,19 @@
+package com.itheima.variable;
+
+public class Demo {
+    public static void main(String[] args) {
+        double price = 18.5;
+        System.out.println(price);
+        price += 1;
+        System.out.println(price);
+        int age = 18;
+        String name = "盖尔";
+        String gender = "男";
+        double height = 170.1;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(height);
+
+    }
+}
